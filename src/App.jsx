45a9b87507efdf11b77b3b6f9c13940a0fac7995
Nikolai Goncharuk <nick.94@bk.ru>
2020-05-08 16:40:49 +1000

    Подключение темы

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { ThemeProvider, responsiveFontSizes } from '@material-ui/core/styles';
+import makeTheme from './theme/theme';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 export default function App() {
+  const theme = responsiveFontSizes(makeTheme);
   return (
-    <div>
-      
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    </ThemeProvider>
   );
 };
