b216a7ad2aeb850b9845c00fdbf9777bab845904
Nikolai Goncharuk <nick.94@bk.ru>
2020-05-11 18:23:22 +1000

    Updates

---
+++ precache-manifest.ccf8b114f6767f3eefe6bb4ece2ee6b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd7aa50dcf8b73950d2b47a6b84a600",
+    "url": "/spa_news_aggregator/index.html"
+  },
+  {
+    "revision": "7d6af39ac621f775036b",
+    "url": "/spa_news_aggregator/static/js/2.e13ef502.chunk.js"
+  },
+  {
+    "revision": "d1554ba1f4dba43b5d7d673828105a05",
+    "url": "/spa_news_aggregator/static/js/2.e13ef502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6aa9c053f75cbe7e274",
+    "url": "/spa_news_aggregator/static/js/main.470d2f45.chunk.js"
+  },
+  {
+    "revision": "cb0e97c6995a61fa7670",
+    "url": "/spa_news_aggregator/static/js/runtime-main.8dd89523.js"
+  }
+]);
